950be2c410ebe2cec50cb26404a837a15061378b
Gleison Gibellato da Silva <ggibellato@gmail.com>
2023-05-30 17:22:54 +0100

    Added tool to import from google spreadsheets

---
+++ tools/ImportGoogleSheets/ImportGoogleSpreadSheets/FinancialModel/Model/Broker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+namespace FinancialModel.Model
+{
+    public class Broker
+    {
+        public string Name { get; }
+        public List<Portifolio> Portifolios { get; } = new List<Portifolio>();
+
+        public Broker(string name)
+        {
+            Name = name;
+        }
+    }
+}
