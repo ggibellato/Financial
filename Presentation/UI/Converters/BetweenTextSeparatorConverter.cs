6a7f04c951ef745a8ee7ad8ee721a3e22b50ebf3
Gleison Gibellato da Silva <ggibellato@gmail.com>
2026-03-14 14:43:10 +0000

    Fix separater on the Asset information

---
+++ Presentation/UI/Converters/BetweenTextSeparatorConverter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Financial.Presentation.UI.Converters;
+
+public class BetweenTextSeparatorConverter : IMultiValueConverter
+{
+    public object Convert(object?[] values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Length < 2)
+        {
+            return string.Empty;
+        }
+
+        var left = values[0]?.ToString();
+        var right = values[1]?.ToString();
+
+        if (string.IsNullOrWhiteSpace(left) || string.IsNullOrWhiteSpace(right))
+        {
+            return string.Empty;
+        }
+
+        return parameter as string ?? " · ";
+    }
+
+    public object[] ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
