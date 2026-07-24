29e9fe34b9dde95c3f241e72abe991dbd038dae4
Gleison Gibellato da Silva <ggibellato@gmail.com>
2026-07-24 18:44:17 +0100

    feat(P14-F02): add opening balance fields to the Bank entity

--- Tests/Financial.CashFlow.Domain.Tests/Entities/BankTests.cs
+++ Tests/Financial.CashFlow.Domain.Tests/Entities/BankTests.cs
@@ -15,6 +15,40 @@ public class BankTests
     }
 
     [Fact]
+    public void Create_DefaultsOpeningBalanceToZeroAndDateToDefault()
+    {
+        var bank = Bank.Create("Chase", roundUpEnabled: true);
+
+        bank.OpeningBalance.Should().Be(0m);
+        bank.OpeningBalanceDate.Should().Be(default(DateOnly));
+    }
+
+    [Fact]
+    public void SetOpeningBalance_UpdatesBothFields()
+    {
+        var bank = Bank.Create("Chase", roundUpEnabled: true);
+        var date = new DateOnly(2026, 7, 1);
+
+        bank.SetOpeningBalance(1250.75m, date);
+
+        bank.OpeningBalance.Should().Be(1250.75m);
+        bank.OpeningBalanceDate.Should().Be(date);
+    }
+
+    [Fact]
+    public void SetOpeningBalance_WithNegativeValue_ThrowsAndLeavesPriorValuesUntouched()
+    {
+        var bank = Bank.Create("Chase", roundUpEnabled: true);
+        bank.SetOpeningBalance(100m, new DateOnly(2026, 7, 1));
+
+        var act = () => bank.SetOpeningBalance(-1m, new DateOnly(2026, 8, 1));
+
+        act.Should().Throw<ArgumentException>();
+        bank.OpeningBalance.Should().Be(100m);
+        bank.OpeningBalanceDate.Should().Be(new DateOnly(2026, 7, 1));
+    }
+
+    [Fact]
     public void Create_WithRoundUpDisabled_AssignsFalse()
     {
         var bank = Bank.Create("Barclays", roundUpEnabled: false);
