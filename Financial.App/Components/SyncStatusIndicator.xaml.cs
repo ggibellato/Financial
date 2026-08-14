8af1807f7c2536c1725f498fb8be26608f320178
Gleison Gibellato da Silva <ggibellato@gmail.com>
2026-08-14 07:53:44 +0100

    feat(P31-F12): add SyncStatusIndicator UserControl to MainWindow

---
+++ Financial.App/Components/SyncStatusIndicator.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Financial.Presentation.App.Components
+{
+    /// <summary>
+    /// Interaction logic for SyncStatusIndicator.xaml
+    /// </summary>
+    public partial class SyncStatusIndicator : UserControl
+    {
+        public SyncStatusIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
